24203e2e4add02a8290d61be045d2fad0d741383
Valmir10 <valmir8@hotmail.se>
2025-05-29 21:51:07 +0200

    Updated Food Delivery Website

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/food-delivery-webbsite/",
+  base: "/food-delivery-webbsite/", // matchar repo-namn
   plugins: [react()],
 });
