8cc916246d88aabdf647571165ac2420d14b4648
Valmir10 <valmir8@hotmail.se>
2025-05-31 17:21:08 +0200

    Updated Food Delivery Website

--- seed.js
+++ seed.js
@@ -9,15 +9,15 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 async function seed() {
   try {
-    console.log("🛠  Kopplar upp mot MongoDB (produktion)...");
+    console.log("🛠  connecting to MongoDB (production)...");
     await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ Uppkopplad mot MongoDB.");
+    console.log("✅ Connected to MongoDB.");
 
     await Product.deleteMany({});
-    console.log("🗑  Alla befintliga produkter i produktionen har tagits bort.");
+    console.log("🗑  All products deleted.");
 
     const cleanedData = productsData.map((prod) => {
       return {
@@ -31,19 +31,16 @@ async function seed() {
       };
     });
 
-    // 3. Bulk‑insert “rensa” arrayen i databasen
     const created = await Product.insertMany(cleanedData);
-    console.log(
-      `✅ Skapade ${created.length} produkter i produktionsdatabasen:`
-    );
+    console.log(`✅ Skapade ${created.length} products in product database:`);
     created.forEach((p) => console.log("   •", p.name));
 
     // 4. Koppla ner
     await mongoose.disconnect();
-    console.log("🛑 Kopplade ner från MongoDB. Klart!");
+    console.log("🛑 connected from MongoDB. Klart!");
     process.exit(0);
   } catch (err) {
-    console.error("❌ Kunde inte köra seed:", err);
+    console.error("❌ could not run seed:", err);
     process.exit(1);
   }
 }
